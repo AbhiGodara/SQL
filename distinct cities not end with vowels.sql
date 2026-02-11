3c2ba67f0573787246e97a3481c5c916ff45e116
Abhishek Godara <167399142+AbhiGodara@users.noreply.github.com>
2026-02-12 05:18:31 +0530

    Add SQL query for cities not ending with vowels
    Query to select distinct cities that do not end with vowels.

---
+++ distinct cities not end with vowels.sql
@@ -0,0 +1,17 @@
+(SELECT
+    CITY
+FROM
+    STATION)
+
+EXCEPT
+
+(SELECT
+    CITY
+FROM
+    STATION
+WHERE
+    CITY LIKE '%a'
+    OR CITY LIKE '%e'
+    OR CITY LIKE '%i'
+    OR CITY LIKE '%o'
+    OR CITY LIKE '%u');
