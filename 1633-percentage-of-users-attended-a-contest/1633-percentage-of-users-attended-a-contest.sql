ca1ebb6bd5616b2cc80b062aa1d55a570f451f70
Abhishek Godara <167399142+AbhiGodara@users.noreply.github.com>
2026-03-17 00:22:14 +0530

    Time: 929 ms (60.27%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -9,3 +9,19 @@ GROUP BY
 ORDER BY
     percentage DESC,
     contest_id ASC;
+
+
+-- SELECT
+--     contest_id,
+--     ROUND((COUNT(r.contest_id)/COUNT(u.user_id))*100, 2) as percentage
+-- FROM
+--     Users as u
+-- LEFT JOIN
+--     Register as r
+-- ON
+--     u.user_id=r.user_id
+-- GROUP BY
+--     contest_id
+-- ORDER BY
+--     percentage DESC,
+--     contest_id ASC;
