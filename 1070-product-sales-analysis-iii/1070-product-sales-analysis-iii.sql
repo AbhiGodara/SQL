b9b340addcafb4db0fe8b8edbc401e0e026eb1e8
Abhishek Godara <167399142+AbhiGodara@users.noreply.github.com>
2026-03-30 07:35:50 +0530

    Time: 660 ms (82.99%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,15 +1,34 @@
 # Write your MySQL query statement below
+-- SELECT
+--     s1.product_id,
+--     s1.year as first_year,
+--     s1.quantity,
+--     s1.price
+-- FROM
+--     Sales as s1
+-- JOIN
+--     (SELECT product_id, MIN(year) as first_year
+--     FROM Sales
+--     GROUP BY product_id) as s2
+-- ON
+--     s1.product_id=s2.product_id
+--     AND s1.year=s2.first_year;
+
+
+WITH new_sales as(
+    SELECT
+        *,
+        rank() OVER(partition by product_id ORDER BY year ASC) as row_num
+    FROM
+        Sales
+)
+
 SELECT
-    s1.product_id,
-    s1.year as first_year,
-    s1.quantity,
-    s1.price
+    product_id,
+    year as first_year,
+    quantity,
+    price
 FROM
-    Sales as s1
-JOIN
-    (SELECT product_id, MIN(year) as first_year
-    FROM Sales
-    GROUP BY product_id) as s2
-ON
-    s1.product_id=s2.product_id
-    AND s1.year=s2.first_year
+    new_sales
+WHERE
+    row_num=1
